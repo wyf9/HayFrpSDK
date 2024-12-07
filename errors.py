4c37f838616bb0abdb1ea75627150f768007ca9c
wyf9 <wyf9@wyf9.top>
2024-12-07 19:08:34 +0800

    《重新发明轮子症》 https://requests.readthedocs.io/projects/cn/zh-cn/latest/

--- errors.py
+++ errors.py
@@ -1,11 +1,48 @@
 # coding: utf-8
 
-class MissingParamError(Exception):
+class HayFrpSDKBaseException(Exception):
+    '''
+    Base Exception
+    '''
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class MissingParamException(HayFrpSDKBaseException):
     '''
     传入参数不足
     '''
-    def __init__(self, msg):
-         self.msg = msg
 
-    def __str__(self):
-        return f'传入参数不足: {self.msg}'
+# --- 下面是根据 api 返回设置的异常
+
+class MissingDataException(HayFrpSDKBaseException):
+    '''
+    数据缺失
+    status: 404
+    message: XXX不能为空.
+    '''
+
+class MissingTypeException(HayFrpSDKBaseException):
+    '''
+    未指定操作(type值未设置)
+    status: 404
+    message: 无用户类型.
+    '''
+
+class WrongPasswdException(HayFrpSDKBaseException):
+    '''
+    密码错误
+    status: 403
+    message: 登录失败，您的密码有误.
+    '''
+
+class UserNotFoundException(HayFrpSDKBaseException):
+    '''
+    用户不存在
+    status: 404
+    message: 用户还没有注册.
+    '''
--- models.py
+++ models.py
@@ -11,26 +11,35 @@ official: https://learn.hayfrp.com/api.html
 使用HayFrp OPENAPI即代表您已同意协议。
 镜像源API协议与主要API协议不同，请仔细阅读。
 '''
+import os
+import json
+import requests
+
 import utils as u
-from errors import *
+import errors as e
+
 
 class account:
+    ''''''
     '''
     # First - 用户账号API
     该API下的全局路径为：/user
     全局请求方式：JSON
     全局请求类型：POST
     '''
-    username = ''  # 用户名
-    password = ''  # 密码
-    csrf = ''  # 鉴权用 CSRF
-    api_base = ''  # api 地址
+    debug: bool = False
+    username: str = ''  # 用户名
+    password: str = ''  # 密码
+    csrf: str = ''  # 鉴权用 CSRF
+    api_base: str = ''  # api 地址
+    timeout: int = None  # 请求超时 (秒)
 
     def __init__(self,
+                 debug: bool = False,
                  username: str = None,
                  password: str = None,
-                 csrf: str = None,
-                 api_base: str = 'https://api.hayfrp.com'
+                 api_base: str = 'https://api.hayfrp.com',
+                 timeout: int = 10
                  ) -> bool:
         '''
         初始化用户 class
@@ -38,4 +47,63 @@ class account:
         @param username: 用户名
         @param password: 密码 *(明文)*
         '''
-        assert (username and password) or csrf, MissingParamError('需要 用户名/密码 或 csrf')
+        assert username and password, e.MissingParamException('需要 username 和 password')
+        self.debug = debug
+        self.username = username
+        self.password = password
+        self.api_base = api_base
+        self.timeout = timeout
+
+    def log(self, msg):
+        if self.debug:
+            print(f'[sdk] {msg}')
+
+    def get(self, api: str) -> tuple[int, dict]:
+        '''
+        get to api
+        '''
+        req = requests.get(
+            url=os.path.join(self.api_base, api),
+            headers={
+                'user-agent': f'requests/{requests.__version__} (compatible; HayFrpSDK; https://github.com/wyf9/HayFrpSDK/)',
+                'waf': 'off'
+            }
+        )
+        try:
+            return (req.status_code, req.json())
+        except requests.exceptions.JSONDecodeError:
+            return (req.status_code, None)
+
+    def post(self, api: str, data: dict) -> tuple[int, dict]:
+        '''
+        post to api
+        '''
+
+        try:
+            req = requests.post(
+                url=os.path.join(self.api_base, api),
+                data=json.dumps(data, ensure_ascii=False),
+                headers={
+                    'user-agent': f'requests/{requests.__version__} (compatible; HayFrpSDK; https://github.com/wyf9/HayFrpSDK/)',
+                    'waf': 'off'
+                }
+            )
+            return (req.status_code, req.json())
+        except requests.exceptions.JSONDecodeError:
+            return (req.status_code, None)
+
+    def login(self):
+        '''
+        使用用户名密码登录, 获取 csrf
+        '''
+        code, data = self.post(api='user', data={
+            'type': 'login',
+            'user': self.username,
+            'passwd': self.password
+        })
+        data['']
+
+    def check_csrf():
+        '''
+        检查 csrf 是否有效, 如无效或空则重新登录
+        '''
